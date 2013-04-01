b0db5b0a771cee3d68a4ca5f66eb1c66840d1e0f
ABODUR <eahmet@gmail.com>
2013-04-01 20:58:15 +0300

    herşey

--- SQLReporter/src/com/sdisoftware/sqlreporter/Main.java
+++ SQLReporter/src/com/sdisoftware/sqlreporter/Main.java
@@ -3,7 +3,7 @@ package com.sdisoftware.sqlreporter;
 import android.app.Activity;
 import android.os.Bundle;
 
-class Main extends Activity{
+public class Main extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
