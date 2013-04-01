940621112a362ef735d750a7592ccb4e2b1840a1
ABODUR <eahmet@gmail.com>
2013-04-01 20:48:44 +0300

    a

---
+++ SQLReporter/src/com/sdisoftware/sqlreporter/Main.java
@@ -0,0 +1,12 @@
+package com.sdisoftware.sqlreporter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+class Main extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
