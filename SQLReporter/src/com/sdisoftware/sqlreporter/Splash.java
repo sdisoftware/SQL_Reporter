d48fb0e513605b48f7981ef5ffbffa04387527bc
ABODUR <eahmet@gmail.com>
2013-04-01 20:47:15 +0300

    edit1

--- SQLReporter/src/com/sdisoftware/sqlreporter/Splash.java
+++ SQLReporter/src/com/sdisoftware/sqlreporter/Splash.java
@@ -1,7 +1,10 @@
 package com.sdisoftware.sqlreporter;
 
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class Splash extends Activity {
@@ -10,6 +13,16 @@ public class Splash extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		
+		final Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            public void run()
+            {
+                // Starts the About Screen Activity
+                startActivity(new Intent(Splash.this,Main.class));
+            }
+        }, 2000L);
 	}
 
 	@Override
